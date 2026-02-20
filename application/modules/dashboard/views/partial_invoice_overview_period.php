27c02013502d76b397ec3a2023fc746ee7c2db72
Chrissie Brown <chrissie@x-tra-designs.org>
2026-02-20 18:14:27 +0100

    fix dashboard period overveiw

--- application/modules/dashboard/views/partial_invoice_overview_period.php
+++ application/modules/dashboard/views/partial_invoice_overview_period.php
@@ -3,22 +3,22 @@
         <!-- see settings/views/partial_settings_general.php -->
             <select name="invoice_overview_period" id="invoice_overview_period"
                 class="form-control simple-select" data-minimum-results-for-search="Infinity">
-                <option value="this_month" <?php check_select($invoice_status_period, 'this_month'); ?>>
+                <option value="this-month" <?php check_select($invoice_status_period, 'this_month'); ?>>
                     <?php _trans('this_month'); ?>
                 </option>
-                <option value="last_month" <?php check_select($invoice_status_period, 'last_month'); ?>>
+                <option value="last-month" <?php check_select($invoice_status_period, 'last_month'); ?>>
                     <?php _trans('last_month'); ?>
                 </option>
-                <option value="this_quarter" <?php check_select($invoice_status_period, 'this_quarter'); ?>>
+                <option value="this-quarter" <?php check_select($invoice_status_period, 'this_quarter'); ?>>
                     <?php _trans('this_quarter'); ?>
                 </option>
-                <option value="last_quarter" <?php check_select($invoice_status_period, 'last_quarter'); ?>>
+                <option value="last-quarter" <?php check_select($invoice_status_period, 'last_quarter'); ?>>
                     <?php _trans('last_quarter'); ?>
                 </option>
-                <option value="this_year" <?php check_select($invoice_status_period, 'this_year'); ?>>
+                <option value="this-year" <?php check_select($invoice_status_period, 'this_year'); ?>>
                     <?php _trans('this_year'); ?>
                 </option>
-                <option value="last_year" <?php check_select($invoice_status_period, 'last_year'); ?>>
+                <option value="last-year" <?php check_select($invoice_status_period, 'last_year'); ?>>
                     <?php _trans('last_year'); ?>
                 </option>
             </select>
