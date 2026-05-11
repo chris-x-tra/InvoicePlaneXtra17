a69933600d1dd4db180832eb1fe7d42e887daa6e
Chrissie Brown <chrissie@x-tra-designs.org>
2026-05-11 11:51:00 +0200

    jwt key added - important for api login

---
+++ application/config/jwt.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// jwt key by chrissie
+$config['jwt_key'] = 'af6530ff0fda64ad82a6ab29cd5d97af383f9a5c3898ebdefd0c07b8153828f6';
